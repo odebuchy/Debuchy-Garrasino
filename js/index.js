287ecf0fe971fbfda52a3f220766652d0c8fd2fe
odebuchy <odebuchy@udesa.edu.ar>
2024-06-11 20:17:41 -0300

    estilos js hombre y mujer

--- js/index.js
+++ js/index.js
@@ -20,9 +20,120 @@ fetch('https://fakestoreapi.com/products/category/women\'s clothing')
     
     sectionRopaMujer.innerHTML= RopaMujer
 
+    sectionRopaMujer.style.flexDirection = 'row'
+    sectionRopaMujer.style.display = 'flex'
+    sectionRopaMujer.style.flexWrap = 'wrap'
+   sectionRopaMujer.style.alignSelf = 'center'
+   sectionRopaMujer.style.justifyContent = 'space-around'
+    
+    }
+    divMujer = document.querySelectorAll('.ropamujerhome div')
+    for (let i = 0; i < divMujer.length; i++) {
+    divMujer[i].style.margin = '10px'
+    divMujer[i].style.paddingTop = '30px'
+    divMujer[i].style.paddingBottom = '10px'
+    divMujer[i].style.paddingLeft = '10px'
+    divMujer[i].style.paddingRight = '10px'
+    divMujer[i].style.width = '20%'
+    divMujer[i].style.border = '1PX solid black'
+    divMujer[i].style.backgroundColor = 'none'
+    divMujer[i].style.borderRadius = '10px'
+    divMujer[i].style.textAlign = "center"
+  }
+   let imagenMujer = document.querySelectorAll('.ropahombrehome div img')
+    for (let i = 0; i < imagenMujer.length; i++) {
+    imagenMujer[i].style.width = '"28%'
+    imagenMujer[i].style.height = '40%'
+    imagenMujer[i].style.textAlign = 'center'
+     imagenMujer[i].style.marginTop = '20px'
+     imagenMujer[i].style.alignSelf = 'center'
+    }
+
+    let tituloMujer = document.querySelectorAll('.ropamujerhome div h2')
+    for (let i = 0; i < tituloMujer.length; i++) {
+      tituloMujer[i].style.fontSize = "115%"
+      tituloMujer[i].style.fontWeight = "400"
+      tituloMujer[i].style.fontFamily ="Bebas Neue", "sans-serif"
+    }
+      let descripcionMujer = document.querySelectorAll('.ropamujerhome div p')
+    for (let i = 0; i < descripcionMujer.length; i++) {
+     descripcionMujer[i].style.fontSize = '85%'
+     descripcionMujer[i].style.fontFamily ="Bebas Neue", "sans-serif"
+   }
+    
+})
+.catch(function(e){
+  console.log(e)
+})
 
 
-}
+fetch('https://fakestoreapi.com/products/category/men\'s clothing')
+.then (function(response){
+    return response.json();
+  })
+  .then (function (data){
+    console.log(data); 
+    let arrayCategoriaHombre = data;
+    let sectionRopaHombre = document.querySelector(`.ropahombrehome`);
+    let RopaHombre = [];
+    
+    for (let i = 0; i <arrayCategoriaHombre.length ; i++) {
+    RopaHombre+=` <div>
+    <h2>${arrayCategoriaHombre[i].title}</h2>
+      <img src="${arrayCategoriaHombre[i].image}" class="im">
+     <p>${arrayCategoriaHombre[i].description}</p>
+     <p>${arrayCategoriaHombre[i].price}</p>
+     <a  href="./producto.html?id=${arrayCategoriaHombre[i].id}">VER MAS</a>
+     </div>`
+      
+    
+    sectionRopaHombre.innerHTML= RopaHombre
+  
+    sectionRopaHombre.style.display = 'flex'
+   sectionRopaHombre.style.flexDirection = 'row'
+    sectionRopaHombre.style.flexWrap = 'wrap'
+   sectionRopaHombre.style.justifyContent = 'space-around'
+   sectionRopaHombre.style.alignSelf = 'center'
+    
+    }
+    divHombre = document.querySelectorAll('.ropahombrehome div')
+    for (let i = 0; i < divHombre.length; i++) {
+      divHombre[i].style.margin = '10px'
+      divHombre[i].style.paddingTop = '30px'
+      divHombre[i].style.paddingBottom = '10px'
+      divHombre[i].style.paddingLeft = '10px'
+      divHombre[i].style.paddingRight = '10px'
+      divHombre[i].style.width = '20%'
+      divHombre[i].style.border = '1PX solid black'
+      divHombre[i].style.backgroundColor = 'none'
+      divHombre[i].style.borderRadius = '10px'
+      divHombre[i].style.textAlign = "center"
+  }
+   let imagenHombre = document.querySelectorAll('.ropahombrehome div img')
+    for (let i = 0; i < imagenHombre.length; i++) {
+    imagenHombre[i].style.width = '"28%'
+      imagenHombre[i].style.height = '40%'
+    imagenHombre[i].style.textAlign = 'center'
+     imagenHombre[i].style.marginTop = '20px'
+     imagenHombre[i].style.alignSelf = 'center'
+    }
+
+    let tituloHombre = document.querySelectorAll('.ropahombrehome div h2')
+    for (let i = 0; i < tituloHombre.length; i++) {
+      tituloHombre[i].style.fontSize = "115%"
+      tituloHombre[i].style.fontWeight = "400"
+      tituloHombre[i].style.fontFamily ="Bebas Neue", "sans-serif"
+    }
+      let descripcionHombre = document.querySelectorAll('.ropahombrehome div p')
+    for (let i = 0; i < descripcionHombre.length; i++) {
+     descripcionHombre[i].style.fontSize = '85%'
+     descripcionHombre[i].style.fontFamily ="Bebas Neue", "sans-serif"
+   
+   }
+    
+})
+.catch(function(e){
+  console.log(e)
 })
 
 
@@ -47,7 +158,52 @@ fetch('https://fakestoreapi.com/products/category/men\'s clothing')
     
     sectionRopaHombre.innerHTML= RopaHombre
 
-}
+
+    sectionRopaHombre.style.display = 'flex'
+   sectionRopaHombre.style.flexDirection = 'row'
+    sectionRopaHombre.style.flexWrap = 'wrap'
+   sectionRopaHombre.style.justifyContent = 'space-around'
+   sectionRopaHombre.style.alignSelf = 'center'
+    
+    }
+    divHombre = document.querySelectorAll('.ropahombrehome div')
+    for (let i = 0; i < divHombre.length; i++) {
+      divHombre[i].style.margin = '10px'
+      divHombre[i].style.paddingTop = '30px'
+      divHombre[i].style.paddingBottom = '10px'
+      divHombre[i].style.paddingLeft = '10px'
+      divHombre[i].style.paddingRight = '10px'
+      divHombre[i].style.width = '20%'
+      divHombre[i].style.border = '1PX solid black'
+      divHombre[i].style.backgroundColor = 'none'
+      divHombre[i].style.borderRadius = '10px'
+      divHombre[i].style.textAlign = "center"
+  }
+   let imagenHombre = document.querySelectorAll('.ropahombrehome div img')
+    for (let i = 0; i < imagenHombre.length; i++) {
+    imagenHombre[i].style.width = '"28%'
+      imagenHombre[i].style.height = '40%'
+    imagenHombre[i].style.textAlign = 'center'
+     imagenHombre[i].style.marginTop = '20px'
+     imagenHombre[i].style.alignSelf = 'center'
+    }
+
+    let tituloHombre = document.querySelectorAll('.ropahombrehome div h2')
+    for (let i = 0; i < tituloHombre.length; i++) {
+      tituloHombre[i].style.fontSize = "115%"
+      tituloHombre[i].style.fontWeight = "400"
+      tituloHombre[i].style.fontFamily ="Bebas Neue", "sans-serif"
+    }
+      let descripcionHombre = document.querySelectorAll('.ropahombrehome div p')
+    for (let i = 0; i < descripcionHombre.length; i++) {
+     descripcionHombre[i].style.fontSize = '85%'
+     descripcionHombre[i].style.fontFamily ="Bebas Neue", "sans-serif"
+   
+   }
+    
+})
+.catch(function(e){
+  console.log(e)
 })
 
 fetch('https://fakestoreapi.com/products/category/jewelery')
@@ -81,7 +237,7 @@ fetch('https://fakestoreapi.com/products/category/jewelery')
 })
 
 divAccesorios = document.querySelectorAll('.accesorioshome div')
-for (let i = 0; i < divMujer.length; i++){
+for (let i = 0; i < divAccesorios.length; i++){
   divAccesorios[i].style.margin = '10px'
   divAccesorios[i].style.paddingTop = '30px'
   divAccesorios[i].style.paddingBottom = '10px'
@@ -115,4 +271,4 @@ for (let index = 0; index < descriptionAccesorios.length; index++) {
   descriptionAccesorios[i].style.fontSize = '85%'
   descriptionAccesorios[i].style.fontFamily = "Bebas Neue", 'sans-serif'
   
-}
+}  
