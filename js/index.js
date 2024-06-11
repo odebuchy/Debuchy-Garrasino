250f6a088603ff3fcdee12d3d85c775cc3d23b81
odebuchy <odebuchy@udesa.edu.ar>
2024-06-10 21:29:13 -0300

    fetch categoria hombre index js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ fetch('https://fakestoreapi.com/products/category/women\'s clothing')
     for (let i = 0; i <arrayCategoriaMujer.length ; i++) {
     RopaMujer+=` <div>
     <h2>${arrayCategoriaMujer[i].title}</h2>
-      <img src="${arrayCategoriaMujer[i].image}" class="im">
+      <img src="${arrayCategoriaMujer[i].image}" class="imagenesMujer">
      <p>${arrayCategoriaMujer[i].description}</p>
      <p>${arrayCategoriaMujer[i].price}</p>
      <a  href="./producto.html?id=${arrayCategoriaMujer[i].id}">VER MAS</a>
@@ -20,7 +20,33 @@ fetch('https://fakestoreapi.com/products/category/women\'s clothing')
     
     sectionRopaMujer.innerHTML= RopaMujer
 
+
+
+}
+})
+
+
+fetch('https://fakestoreapi.com/products/category/men\'s clothing')
+.then (function(response){
+    return response.json();
+  })
+  .then (function (data){
+    console.log(data); 
+    let arrayCategoriaHombre = data;
+    let sectionRopaHombre = document.querySelector(`.ropahombrehome`);
+    let RopaHombre = [];
     
+    for (let i = 0; i <arrayCategoriaHombre.length ; i++) {
+    RopaHombre+=` <div>
+    <h2>${arrayCategoriaHombre[i].title}</h2>
+      <img src="${arrayCategoriaHombre[i].image}" class="imagenesHombre">
+     <p>${arrayCategoriaHombre[i].description}</p>
+     <p>${arrayCategoriaHombre[i].price}</p>
+     <a  href="./producto.html?id=${arrayCategoriaHombre[i].id}">VER MAS</a>
+     </div>`
+      
+    
+    sectionRopaHombre.innerHTML= RopaHombre
 
 }
 })
