a0661c64e12caf421a442183bd0bf86332110f23
odebuchy <odebuchy@udesa.edu.ar>
2024-06-12 23:35:07 -0300

    header category js

---
+++ js/category.js
@@ -0,0 +1,25 @@
+fetch('https://fakestoreapi.com/products/categories')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log("data", data)
+        let arrayCategorias = data;
+        let headerHome = document.querySelector('.sectionheader')
+        let header = [];
+
+        headerHome.innerHTML =
+            `<li><a href="./index.html?">Home</a></li>
+  <li><a href="./category.html?categories=${arrayCategorias.category = "jewelery"}">Accesorios</a></li>
+  <li><a href="./category.html?categories=${arrayCategorias.category = "electronics"}">Electronica</a></li>
+  <li><a href="./category.html?categories=${arrayCategorias.category = "men's clothing"}">Hombre</a></li>
+  <li><a href="./category.html?categories=${arrayCategorias.category = "women's clothing"}">Mujer</a></li>
+  <li><a href="./cart.html?">Carrito</a></li>
+  <li><a href="./login.html?">Carrito</a></li>
+  <li><a href="./register.html?">Registro</a></li>`
+
+
+    })
+    .catch(function (e) {
+        console.log(e)
+    })
