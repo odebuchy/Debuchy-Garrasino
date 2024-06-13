5a3e5318f6b38e0fe8dff35980eba25890386b5c
odebuchy <odebuchy@udesa.edu.ar>
2024-06-12 21:11:24 -0300

    header home en index js que lleve a las distintas categorias

--- js/index.js
+++ js/index.js
@@ -1,3 +1,34 @@
+fetch ('https://fakestoreapi.com/products/categories')
+.then (function(response){
+  return response.json ();
+})
+.then (function(data){
+  console.log("data",data)
+  let arrayCategorias= data;
+  let headerHome = document.querySelector ('.sectionheader')
+  let header = [];
+
+  headerHome.innerHTML = 
+  `<li><a href="./index.html?">Home</a></li>
+  <li><a href="./category.html?categories=${arrayCategorias.category="jewelery"}">Accesorios</a></li>
+  <li><a href="./category.html?categories=${arrayCategorias.category="electronics"}">Electronica</a></li>
+  <li><a href="./category.html?categories=${arrayCategorias.category="men's clothing"}">Hombre</a></li>
+  <li><a href="./category.html?categories=${arrayCategorias.category="women's clothing"}">Mujer</a></li>
+  <li><a href="./cart.html?">Carrito</a></li>
+  <li><a href="./login.html?">Carrito</a></li>
+  <li><a href="./register.html?">Registro</a></li>`
+ 
+  
+})
+.catch(function(e){
+ console.log(e)
+});
+
+
+
+
+
+
 fetch('https://fakestoreapi.com/products/category/women\'s clothing')
 .then (function(response){
     return response.json();
