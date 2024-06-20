3a52408741d435ac2cb422ce5f00367d0994352f
emiliagarrasino <egarrasino@udesa.edu.ar>
2024-06-20 16:07:10 -0300

    intentando de hacer funcionar el carrito js

--- js/cart.js
+++ js/cart.js
@@ -8,36 +8,7 @@ fetch('https://fakestoreapi.com/products/categories')
     let arrayCategorias = data;
     let headerHome = document.querySelector('.sectionheader')
     let header = [];
-
-    headerHome.innerHTML =
-        `<li><a href="./index.html?">Home</a></li>
-<li><a href="./category.html?categories=${arrayCategorias.category = "jewelery"}">Accesorios</a></li>
-<li><a href="./category.html?categories=${arrayCategorias.category = "electronics"}">Electronica</a></li>
-<li><a href="./category.html?categories=${arrayCategorias.category = "men's clothing"}">Hombre</a></li>
-<li><a href="./category.html?categories=${arrayCategorias.category = "women's clothing"}">Mujer</a></li>
-<li><a href="./cart.html?">Carrito</a></li>
-<li><a href="./login.html?">Login</a></li>
-<li><a href="./register.html?">Registro</a></li>`
-
-
 })
 .catch(function (e) {
     console.log(e)
 })
-
-
-
-
-console.log("categoria", categoria)
-
-    
-    
-    
-    
-    
-    let datosCart = localStorage.getItem ("arrayCarrito")
-    
-    let datosParse = JSON.parse (datosCart)
-    console.log (datosParse)
-    
-    
