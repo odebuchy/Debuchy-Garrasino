dcd89ceb397acd76908d12ba6716b2536512c2fe
emiliagarrasino <egarrasino@udesa.edu.ar>
2024-06-21 13:41:17 -0300

    desarrollo del punto 4 en el cart js

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let cartContainer = document.querySelector('.cart-container');
+    let arrayCart = JSON.parse(localStorage.getItem('arrayCarrito')) || [];
 
-fetch('https://fakestoreapi.com/products/categories')
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    console.log("data", data)
-    let arrayCategorias = data;
-    let headerHome = document.querySelector('.sectionheader')
-    let header = [];
-})
-.catch(function (e) {
-    console.log(e)
-})
+    if (arrayCart.length === 0) {
+        cartContainer.innerHTML = '<p>Su carrito está vacío</p>';
+    } else {
+        arrayCart.forEach(productId => {
+            fetch(`https://fakestoreapi.com/products/${productId}`)
+                .then(res => res.json())
+                .then(data => {
+                    cartContainer.innerHTML += `
+                        <div class="product">
+                            <h2>${data.title}</h2>
+                            <img src="${data.image}" alt="${data.title}">
+                            <p>Price: $${data.price}</p>
+                            <p>${data.description}</p>
+                        </div>`;
+                })
+                .catch(e => console.log(e));
+        });
+    }
+});
+
+
