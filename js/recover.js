f2c29f42c0bb9261b1b13bde9eaa9fb04f8b7cc8
emiliagarrasino <egarrasino@udesa.edu.ar>
2024-06-24 11:52:19 -0300

    si el form de recuperar contra cumple con todo que aparezca un texto y un link para volver al log in

--- js/recover.js
+++ js/recover.js
@@ -5,9 +5,9 @@ let errorMail = document.querySelector('.errorMail');
 let checkbox = document.querySelector('#checkbox');
 let errorCheckbox = document.querySelector('.errorCheckbox');
 let avisoMail = document.querySelector (".aviso")
-let irALogin = document.querySelector (".ir")
+let irALogin = document.querySelector ("#ir")
 
-irALogin.style.display= "none"
+irALogin.style.display =`none`
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -15,11 +15,11 @@ form.addEventListener('submit', function (e) {
         errorMail.innerText = 'Por favor escriba su email.';
     } else if (!checkbox.checked) {
         errorCheckbox.innerText = 'Por favor acepte el campo "Quiero recuperar mi contraseña."'
+    } else if (this.submit){
+        avisoMail.innerText = 'Recibiras un mail con las instrucciones para recuperar tu contraseña.'
+        irALogin.style.display=`block`
     } else {
         user.email = email.value;
         localStorage.setItem('user', JSON.stringify(user))
-        this.submit();
-        document.location.href = 'login.html'
     }
-
 })
