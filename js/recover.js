23a9fbfb0dc24f1b409088256f550ae482f5552d
odebuchy <odebuchy@udesa.edu.ar>
2024-06-24 11:09:39 -0300

    recover

--- js/recover.js
+++ js/recover.js
@@ -4,6 +4,10 @@ let email = document.querySelector('#email');
 let errorMail = document.querySelector('.errorMail');
 let checkbox = document.querySelector('#checkbox');
 let errorCheckbox = document.querySelector('.errorCheckbox');
+let avisoMail = document.querySelector (".aviso")
+let irALogin = document.querySelector (".ir")
+
+irALogin.style.display= "none"
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
@@ -17,6 +21,5 @@ form.addEventListener('submit', function (e) {
         this.submit();
         document.location.href = 'login.html'
     }
-
 
 })
