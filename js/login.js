7814c9ab7c985d13041359089368be8b6eb0824f
odebuchy <odebuchy@udesa.edu.ar>
2024-06-24 17:00:34 -0300

    agregue header dinamico en login y register js

--- js/login.js
+++ js/login.js
@@ -1,3 +1,36 @@
+
+fetch('https://fakestoreapi.com/products/categories')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log("data", data)
+    let arrayCategorias = data;
+    let headerHome = document.querySelector('.sectionheader')
+    let header = [];
+
+    headerHome.innerHTML =
+      `<div class= listaHeader>
+<li><a href="./index.html?">Home</a></li>
+<li><a href="./category.html?categories=${arrayCategorias.category = "jewelery"}">Accesorios</a></li>
+<li><a href="./category.html?categories=${arrayCategorias.category = "electronics"}">Electronica</a></li>
+<li><a href="./category.html?categories=${arrayCategorias.category = "men's clothing"}">Hombre</a></li>
+<li><a href="./category.html?categories=${arrayCategorias.category = "women's clothing"}">Mujer</a></li>
+<li><a href="./cart.html?">Carrito</a></li>
+<li><a href="./login.html?">Login</a></li>
+<li><a href="./register.html?">Registro</a></li>
+</div> `
+
+
+  })
+  .catch(function (e) {
+    console.log(e)
+  });
+
+
+
+
+
 let form = document.querySelector('form');
 let mail = document.querySelector('#mail');
 let pass = document.querySelector('#pass');
