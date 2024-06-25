df688061ae3769525d91fee8ff91d54855d0465f
odebuchy <odebuchy@udesa.edu.ar>
2024-06-24 21:34:59 -0300

    borre cosas que eran innecesarias en logout y saludo js

--- js/logout.js
+++ js/logout.js
@@ -42,36 +42,8 @@ if (datosParseUserLogout) {
         .catch(function (e) {
             console.log("error1", e)
         })
-} else {
+} else {}
 
-    fetch('https://fakestoreapi.com/products/categories')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log("data", data)
-            let arrayCategorias = data;
-            let headerHome = document.querySelector('.sectionheader')
-            let header = [];
-
-            headerHome.innerHTML =
-                `<div class= listaHeader>
-<li><a href="./index.html?">Home</a></li>
-<li><a href="./category.html?categories=${arrayCategorias.category = "jewelery"}">Accesorios</a></li>
-<li><a href="./category.html?categories=${arrayCategorias.category = "electronics"}">Electronica</a></li>
-<li><a href="./category.html?categories=${arrayCategorias.category = "men's clothing"}">Hombre</a></li>
-<li><a href="./category.html?categories=${arrayCategorias.category = "women's clothing"}">Mujer</a></li>
-<li><a href="./cart.html?">Carrito</a></li>
-<li><a href="./login.html?">Login</a></li>
-<li><a href="./register.html?">Registro</a></li>
-</div> `
-
-        })
-        .catch(function (e) {
-            console.log(e)
-        });
-
-}
 
 
 
